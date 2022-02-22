ae19da59c719f0f4da70875acf1f6f84d8deb691
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-22 17:53:41 +0300

    Update test1 v2 complete

--- mytests/test1.js
+++ mytests/test1.js
@@ -1,3 +1,13 @@
-const stringLength = string => string.length;
+const stringLength = string => {
+  let myStringLength =  string.length;
+
+  if(myStringLength < 1) {
+    throw "Your string cannot be less than 1 character";
+  } else if(myStringLength > 10) {
+    throw "Your string length cannot be more than 10 characters";
+  } else {
+    return myStringLength;
+  }
+};
 
 module.exports = stringLength;
--- mytests/test1.test.js
+++ mytests/test1.test.js
@@ -3,5 +3,10 @@ const stringLength = require('./test1');
 
 test('Check if string length matches the actual string length', () => {
   let str = stringLength(string);
-  expect(stringLength('Jest')).not.toEqual(str);
+  expect(stringLength('test')).not.toEqual(str);
 });
+
+test('Check if the string length is too long or short', () => {
+  let mylength = stringLength(string);
+  expect(stringLength('test')).not.toBe(mylength);
+})
